5759facac05af63e9a6b04667108df41ed62dadc
soccerdroid <mguarand@espol.edu.ec>
2018-01-12 16:08:43 -0500

    analisis de edades

--- analisis_edades.py
+++ analisis_edades.py
@@ -7,22 +7,39 @@ df=pd.read_csv('201705-citibike-tripdata.csv')
 #print(df.isnull().sum()) 
 #df.dropna(axis=0, how='all')
 df.rename(columns = {'start station id':'start_station_id'}, inplace = True)
+df.rename(columns = {'birth year':'birth_year'}, inplace = True)
+
 #print(df.start_station_id.nunique())
 
-df = df[np.isfinite(df['birth year'])]
-#print(df.isnull().sum()) 
-df["age"]=2017-df["birth year"]
-df[['Date','Time']] = df.starttime.str.split(expand=True)
-grupo_fechas=df[["Date","bikeid"]].groupby("Date").count()
-grupo_fechas.reset_index(inplace=True)
+df = df[np.isfinite(df['birth_year'])]
+df = df[df.birth_year >1927]
 
-grupo_fechas['day_of_week'] =pd.to_datetime(grupo_fechas['Date']).dt.weekday_name
-grupo_fechas.drop
-#print(grupo_fechas)
-#df.groupby(['start_station_id','age']).sum()
-#plt.figure(); grupo_fechas.plot();
-grupo_fechas.rename(columns = {'bikeid':'trip_counts'}, inplace = True)
+#print(df.isnull().sum()) 
+df["age"]=2017-df["birth_year"]
+df["tripduration"]=df["tripduration"]/60
+df[['Date','Time']] = df.starttime.str.split(expand=True)
+# grupo_fechas=df[["Date","bikeid"]].groupby("Date").count()
+grupo_edades=df[["age","tripduration"]].groupby("age").mean()
+grupo_edades.rename(columns = {'tripduration':'tripduration_mean'}, inplace = True)
+grupo_edades.reset_index(inplace=True)
+print(grupo_edades)
+
+# # print(grupo_edades)
+# grupo_fechas.reset_index(inplace=True)
+
+# grupo_fechas['day_of_week'] =pd.to_datetime(grupo_fechas['Date']).dt.weekday_name
+# grupo_fechas.drop
+# #print(grupo_fechas)
+# #df.groupby(['start_station_id','age']).sum()
+# #plt.figure(); grupo_fechas.plot();
+# grupo_fechas.rename(columns = {'bikeid':'trip_counts'}, inplace = True)
+
 plt.figure();
-df.set_index('Date')
-grupo_fechas.plot(title='Cantidad de recorridos en Mayo')
+plt.scatter(grupo_edades["age"], grupo_edades["tripduration_mean"],alpha=.1)
+
+# grupo_fechas=grupo_fechas.set_index('Date')
+# print(grupo_fechas)
+# grupo_fechas.plot(title='Cantidad de recorridos en Mayo')
+# #plt.xticks(np.arange(1, 31, 1))
+# #plt.set_xtickslabels(df["day_of_week"].values)
 plt.show()
